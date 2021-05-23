f06196aa6ddec6a4580b0208587112b9967288ac
Sammily <sammily03@yandex.ru>
2021-05-23 21:54:36 +0300

    m

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { ChartsModule } from 'ng2-charts';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import {SecondComponent} from './second-r/second-r.component'
-
+import { DashboardComponent} from './dashboard/dashboard.component';
+import {SecondComponent} from './second-r/second-r.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import {MatButtonModule} from '@angular/material/button';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatRadioModule} from '@angular/material/radio';
+import {FormsModule} from '@angular/forms';
 //import { HighchartsChartModule } from 'highcharts-angular';
 
 @NgModule({
@@ -18,7 +26,16 @@ import {SecondComponent} from './second-r/second-r.component'
     BrowserModule,
     AppRoutingModule,
     ChartsModule,
-    //HighchartsChartModule
+    BrowserAnimationsModule,
+    //HighchartsChartModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatCardModule,
+    MatCheckboxModule,
+    FormsModule,
+    MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { SyncAsync } from '@angular/compiler/src/util';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { ChartDataSets, ChartOptions, ChartType ,ChartConfiguration} from 'chart.js';
+import { chart } from 'highcharts';
 import { Color, BaseChartDirective, Label } from 'ng2-charts';
+
 
 
 @Component({
@@ -10,10 +13,12 @@ import { Color, BaseChartDirective, Label } from 'ng2-charts';
 })
 export class DashboardComponent  {
 
-
   public lineChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Sensor 1' },
+    { data: [25, 39, 40, 51, 66, 75, 90], label: 'Sensor 2' },
+    { data: [15, 49, 20, 71, 86, 95, 60], label: 'Sensor 3' },
   ];
+
   public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
   public lineChartOptions: (ChartOptions ) = {
     responsive: true,
@@ -24,32 +29,42 @@ export class DashboardComponent  {
       backgroundColor: 'rgba(255,0,0,0.3)',
     },
   ];
+ 
   public lineChartLegend = true;
   public lineChartType: ChartType="line";
   public lineChartPlugins = [];
 
+
+  @ViewChild(BaseChartDirective) private chart!: BaseChartDirective;
+
   constructor() { }
-
-  ngOnInit() {
+  ngOnInit(): void {
   }
 
-  public changeColor(): void {
-    this.lineChartColors[0].borderColor = 'green';
-    this.lineChartColors[0].backgroundColor = `rgba(0, 255, 0, 0.3)`;
+    public changeColor(): void {
+    this.lineChartColors[0].borderColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    this.lineChartColors[0].backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
   }
-  public changeTypeBar(): void {
-       this.lineChartType= 'bar';
-     }
+
   
-public changeTypeLine(): void {
-  this.lineChartType= 'line';
+public changeType(): void {
+  this.lineChartType = this.lineChartType === 'bar' ? 'line' : 'bar';
 }
 
 
 public changeData(): void {
   this.lineChartData=[
-    { data: [Math.round(Math.random() * 100),Math.round(Math.random() * 100), Math.round(Math.random() * 100), Math.round(Math.random() * 100), Math.round(Math.random() * 100), Math.round(Math.random() * 100), Math.round(Math.random() * 100)], label: 'Series B' }
+    { data: [this.randomNumber(),this.randomNumber(),this.randomNumber(),this.randomNumber(),this.randomNumber(), this.randomNumber(),this.randomNumber()], label: 'Series B' }
   ];
+}
+
+public randomNumber(): number{
+  return Math.round(Math.random() * 100);
+}
+
+public hideOne(): void {
+  const isHidden = this.chart.isDatasetHidden(1);
+  this.chart.hideDataset(1, !isHidden);
 }
 
 
