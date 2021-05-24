b557b6608d812de66c6af9a8f0bcff69bb64a360
Dasha <sammilywow@gmail.com>
2021-05-24 16:31:26 +0300

    hide+colour

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,8 +4,6 @@ import { ChartDataSets, ChartOptions, ChartType ,ChartConfiguration} from 'chart
 import { chart } from 'highcharts';
 import { Color, BaseChartDirective, Label } from 'ng2-charts';
 
-
-
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -16,8 +14,11 @@ export class DashboardComponent  {
   public lineChartData: ChartDataSets[] = [
     { data: [65, 59, 80, 81, 56, 55, 40], label: 'Sensor 1' },
     { data: [25, 39, 40, 51, 66, 75, 90], label: 'Sensor 2' },
-    { data: [15, 49, 20, 71, 86, 95, 60], label: 'Sensor 3' },
+    { data: [15, 49, 20, 71, 86, 95, 60], label: 'Sensor 3' }
   ];
+
+  
+
 
   public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
   public lineChartOptions: (ChartOptions ) = {
@@ -25,8 +26,16 @@ export class DashboardComponent  {
   };
   public lineChartColors: Color[] = [
     {
-      borderColor: 'black',
+      borderColor: 'green',
+      backgroundColor: 'rgba(0,128,0,0.3)',
+    },
+    {
+      borderColor: 'red',
       backgroundColor: 'rgba(255,0,0,0.3)',
+    },
+    {
+      borderColor: 'blue',
+      backgroundColor: 'rgba(0,0,255,0.3)',
     },
   ];
  
@@ -42,9 +51,29 @@ export class DashboardComponent  {
   }
 
     public changeColor(): void {
-    this.lineChartColors[0].borderColor = '#'+Math.floor(Math.random()*16777215).toString(16);
-    this.lineChartColors[0].backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+  
+    /*this.lineChartColors[0].borderColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    //this.lineChartColors[0].backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    this.lineChartColors[1].borderColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    this.lineChartColors[1].backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    this.lineChartColors[2].borderColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    this.lineChartColors[2].backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+*/
+  this.lineChartColors[0].borderColor = '#'+this.random1();
+  this.lineChartColors[0].backgroundColor="rgb(" + this.random2() + "," + this.random2() + "," + this.random2() + ",0.3)";
+  this.lineChartColors[1].borderColor = '#'+this.random1();
+  this.lineChartColors[1].backgroundColor="rgb(" + this.random2() + "," + this.random2() + "," + this.random2() + ",0.3)";
+  this.lineChartColors[2].borderColor = '#'+this.random1();
+  this.lineChartColors[2].backgroundColor="rgb(" + this.random2() + "," + this.random2() + "," + this.random2() + ",0.3)";
+   
   }
+  public random1(): string{
+    return Math.floor(Math.random()*16777215).toString(16);
+  }
+  public random2(): number{
+    return Math.floor(Math.random() * 256);
+  }
+
 
   
 public changeType(): void {
@@ -54,7 +83,9 @@ public changeType(): void {
 
 public changeData(): void {
   this.lineChartData=[
-    { data: [this.randomNumber(),this.randomNumber(),this.randomNumber(),this.randomNumber(),this.randomNumber(), this.randomNumber(),this.randomNumber()], label: 'Series B' }
+    { data: [this.randomNumber(),this.randomNumber(),this.randomNumber(),this.randomNumber(),this.randomNumber(), this.randomNumber(),this.randomNumber()], label: 'Series A' },
+    { data: [this.randomNumber(),this.randomNumber(),this.randomNumber(),this.randomNumber(),this.randomNumber(), this.randomNumber(),this.randomNumber()], label: 'Series B' },
+    { data: [this.randomNumber(),this.randomNumber(),this.randomNumber(),this.randomNumber(),this.randomNumber(), this.randomNumber(),this.randomNumber()], label: 'Series C' }
   ];
 }
 
@@ -62,9 +93,9 @@ public randomNumber(): number{
   return Math.round(Math.random() * 100);
 }
 
-public hideOne(): void {
-  const isHidden = this.chart.isDatasetHidden(1);
-  this.chart.hideDataset(1, !isHidden);
+public hideOne(num:number): void {
+  const isHidden = this.chart.isDatasetHidden(num);
+  this.chart.hideDataset(num, !isHidden);
 }
 
 
